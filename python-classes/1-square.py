364367c399e5c6dafe8c55ea1904b405073ace18
Lorenzo Anselme <lorenzoanselme@proton.me>
2026-01-28 08:45:14 +0000

    Fix : Modify the doc

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""This module defines a Square class that represents a square with a given size."""
+"""This module defines a Square class."""
 
 
 class Square:
-    """This class represents a square defined by its size attribute."""
+    """This class represents a square with a size attribute."""
 
     def __init__(self, size=0):
-        """Initialize a new Square instance with a given size value."""
+        """Initialize a new Square instance with a given size."""
         self.__size = size
