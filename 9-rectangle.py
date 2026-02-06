3b062ce84ea7781d4d8562a7abf2fbf64624643e
Lorenzo Anselme <lorenzoanselme@proton.me>
2026-02-06 14:47:01 +0000

    Feat : Add a class Rectangle that inherits from BaseGeometry

---
+++ 9-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Module that defines a Rectangle class inheriting from BaseGeometry.
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle class that inherits from BaseGeometry.
+    Represents a rectangle with width and height.
+    """
+
+    def __init__(self, width, height):
+        """
+        Initialize a Rectangle instance.
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        Calculate the area of the rectangle.
+        Returns:
+            int: The area of the rectangle.
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Return the string representation of the rectangle.
+        Returns:
+            str: The string representation in the format [Rectangle]
+            <width>/<height>.
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
