c00ff21e7dfd9835de088c1097cfb04f47b1c928
Lorenzo Anselme <lorenzoanselme@proton.me>
2026-02-06 14:49:13 +0000

    Feat : Add a class Square that inherits from Rectangle

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module that defines a Rectangle class inheriting from BaseGeometry.
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class that inherits from Rectangle.
+    Represents a square with a given size.
+    """
+
+    def __init__(self, size):
+        """
+        Initialize a Square instance.
+        Args:
+            size (int): The size of the square.
+        """
+
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """
+        Calculate the area of the square.
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size * self.__size
