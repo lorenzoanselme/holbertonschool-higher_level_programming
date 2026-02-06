b04edeaf561407dd5d6a4ad79078150a4d078685
Lorenzo Anselme <lorenzoanselme@proton.me>
2026-02-06 14:46:11 +0000

    Feat : Add a class Rectangle that inherits from BaseGeometry

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module that defines a Rectangle class inheriting from BaseGeometry.
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle class that inherits from BaseGeometry.
+    Represents a rectangle with width and height.
+    """
+
+    def __init__(self, width, height):
+        """
+        Initialize a Rectangle instance.
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
