f70a82d7bb4328158d904833b378a27532937e33
Lorenzo Anselme <lorenzoanselme@proton.me>
2026-02-11 09:11:28 +0000

    Feat : Add a function that returns an object (Python data structure) represented by a JSON string

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module file that contains the function from_json_string
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Returns an object represented by a JSON string
+    """
+    return json.loads(my_str)
