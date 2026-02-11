2c49f91046b84f4814e618c1cd1d532221be9258
Lorenzo Anselme <lorenzoanselme@proton.me>
2026-02-11 09:29:13 +0000

    Feat : Add a a class Student that defines a student

---
+++ python-input_output/10-student.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Module that defines a Student class with attributes first_name, last_name, and
+age,
+and a method to_json that returns the dictionary representation of the Student
+instance.
+"""
+
+
+class Student:
+    """
+    Defines a Student class with attributes first_name, last_name, and age,
+    and a method to_json that returns the dictionary representation of the
+    Student instance.
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance with the given first_name, last_name,
+        and age.
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns the dictionary representation of the Student instance.
+        Args:
+            attrs (list): Optional list of attribute names to retrieve.
+                         If None, all attributes are retrieved.
+        Returns:
+            dict: Dictionary representation of the Student instance.
+        """
+        if attrs is None:
+            return self.__dict__
+        
+        result = {}
+        for attr in attrs:
+            if isinstance(attr, str) and hasattr(self, attr):
+                result[attr] = getattr(self, attr)
+        return result
