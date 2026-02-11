65bc39c5df6af10b91bed64f983c25cbb410637a
Lorenzo Anselme <lorenzoanselme@proton.me>
2026-02-11 08:56:48 +0000

    Feat : Add a function that reads a text file (UTF8) and prints it to stdout

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module file that contains the function read_file
+"""
+
+def read_file(filename=""):
+    """
+    Reads text file and prints it to stdout
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
