90d397311bcdc756526ecce57e04f708e2c167aa
Lorenzo Anselme <lorenzoanselme@proton.me>
2026-02-11 08:58:36 +0000

    Fix file 0 for pycode style

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,9 +3,10 @@
 Module file that contains the function read_file
 """
 
+
 def read_file(filename=""):
     """
-    Reads text file and prints it to stdout
+    Reads text file (UTF-8) and prints it to stdout
     """
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
