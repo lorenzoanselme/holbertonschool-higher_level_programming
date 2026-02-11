591b7e392603b40da22d3879f12fdebe790b86d8
Lorenzo Anselme <lorenzoanselme@proton.me>
2026-02-11 09:24:23 +0000

    Feat : Add a class Student that defines a student

---
+++ python-input_output/9-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module that defines a Student class with attributes first_name, last_name, and
+age,
+and a method to_json that returns the dictionary representation of the Student
+instance.
+"""
+
+
+class Student:
+    """
+    Defines a Student class with attributes first_name, last_name, and age,
+    and a method to_json that returns the dictionary representation of the
+    Student instance.
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance with the given first_name, last_name,
+        and age.
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Returns the dictionary representation of the Student instance.
+        """
+        return self.__dict__
