5c4e61bc4eb73f466f2f767c738a50e59e68a014
Lorenzo Anselme <lorenzoanselme@proton.me>
2026-03-02 11:45:43 +0000

    Feat : Create SQL script to define first_table in the current database

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Script that creates the table first_table in the current database
+-- Create table first_table if it does not exist
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
