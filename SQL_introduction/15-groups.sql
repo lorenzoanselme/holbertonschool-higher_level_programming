6e43a40671147562958d03b66a06a7bed2c38cf7
Lorenzo Anselme <lorenzoanselme@proton.me>
2026-03-05 12:26:30 +0000

    Feat : Add script to list the number of records with the same score in second_table

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+-- Script that lists the number of records with the same score in second_table
+-- Group scores and count occurrences
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC;
