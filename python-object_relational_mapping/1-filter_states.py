45777f60d50983c381266a948f0011d978953db1
Lorenzo Anselme <lorenzoanselme@proton.me>
2026-03-06 10:11:50 +0000

    Fix : corriger la documentation et ajuster la requête SQL pour garantir la recherche des états commençant par N

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Lists all states starting with N from the database"""
+"""Lists all states starting with N from the database."""
 
 import MySQLdb
 from sys import argv
@@ -11,13 +11,11 @@ if __name__ == "__main__":
         port=3306,
         user=argv[1],
         passwd=argv[2],
-        db=argv[3],
-        charset="utf8"
+        db=argv[3]
     )
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-
+    cur.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
 
     for row in rows:
