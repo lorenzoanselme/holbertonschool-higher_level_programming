24d09ea068adef4f6fdd247cdde8aed38c354342
Lorenzo Anselme <lorenzoanselme@proton.me>
2026-03-06 10:05:00 +0000

    Fix : mettre à jour la documentation pour préciser que le script liste tous les états d'une base de données et simplifier l'importation de sys

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-"""Lists all states starting with 'N' from hbtn_0e_0_usa."""
+"""Lists all states starting with 'N' from the database."""
+
 import MySQLdb
-import sys
+from sys import argv
 
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
     )
+
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
+
     for row in rows:
         print(row)
+
     cur.close()
     db.close()
