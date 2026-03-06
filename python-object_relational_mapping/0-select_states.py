7050c3791bbc40e9a03ef95dc67d8d9fc5faa74c
Lorenzo Anselme <lorenzoanselme@proton.me>
2026-03-06 09:50:25 +0000

    Fix : corriger le nom de la variable de connexion à la base de données dans le script 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,9 +3,8 @@
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    conn = MySQLdb.connect(
+    db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=sys.argv[1],
@@ -13,12 +12,11 @@ if __name__ == "__main__":
         db=sys.argv[3]
     )
 
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id")
 
-    rows = cur.fetchall()
-    for row in rows:
+    for row in cur.fetchall():
         print(row)
 
     cur.close()
-    conn.close()
+    db.close()
