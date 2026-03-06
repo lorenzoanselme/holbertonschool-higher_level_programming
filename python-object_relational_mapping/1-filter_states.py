1425f761e8f1e477cded3f70faac7857998804fc
Lorenzo Anselme <lorenzoanselme@proton.me>
2026-03-06 10:13:36 +0000

    Fix : corriger la requête SQL pour sélectionner uniquement les colonnes id et name dans le script 1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,10 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
+    cur.execute(
+        "SELECT id, name FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+    )
+
     rows = cur.fetchall()
 
     for row in rows:
