9d28158ffd189cd3093c4bd1d9238f689fcefdc7
Lorenzo Anselme <lorenzoanselme@proton.me>
2026-03-06 09:39:16 +0000

    Feat : Add  a Python file similar to model_state.py named model_city.py that contains the class definition of a City, add a script 14-model_city_fetch_by_state.py that prints all City objects from the database hbtn_0e_14_usa

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Lists all City objects from the database hbtn_0e_14_usa"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password, db_name),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    cities = session.query(City).order_by(City.id).all()
+
+    for city in cities:
+        state_name = session.query(State).filter(State.id == city.state_id).first().name
+        print(f"{state_name}: ({city.id}) {city.name}")
+
+    session.close()
